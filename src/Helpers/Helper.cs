9827f45a521a3fc8706a53753b47767847371652
isabella-projects <damyan4oy@gmail.com>
2024-08-26 23:48:50 +0300

    @method Output now accepts any type

--- src/Helpers/Helper.cs
+++ src/Helpers/Helper.cs
@@ -5,12 +5,17 @@ namespace LearnCSharp.Helpers
     public static class Helper
     {
         public static void Output(
-            string message,
+            object message,
             [CallerMemberName] string methodName = "",
             [CallerFilePath] string filePath = "")
         {
             string className = Path.GetFileNameWithoutExtension(filePath);
             Console.WriteLine($"[{className}: {methodName}]: {message}");
+        }
+
+        public static void BREAK()
+        {
+            Console.WriteLine();
         }
     }
 }
